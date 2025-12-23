72da245fb1c9cd87bc48c042c6757a9fff303e47
Ngamlubou Ringkangmai <ngamluboungamlubou@gmail.com>
2025-12-23 14:56:14 +0530

    Implement DataStore with caching for JSON data

---
+++ Data Store.js
@@ -0,0 +1,15 @@
+/* datastore.js */
+
+const DataStore = {
+  cache: {},
+
+  async get(listName) {
+    if (this.cache[listName]) {
+      return this.cache[listName]; }
+
+    const response = await fetch(`data/${listName}.json`);
+    const data = await response.json();
+
+    this.cache[listName] = data;
+    return data; }
+};
